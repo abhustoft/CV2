b33498eea69424fa9fb4938a7296917b286bb49f
Alf Bjørn Hustoft <alfbjorn.hustoft@pearson.com>
2016-01-11 11:17:48 +0100

    Basic React set up

--- client-src/app/main.js
+++ client-src/app/main.js
@@ -4,3 +4,13 @@ import {myGithubRepos} from './my_github_repos';
 myGithubRepos('abhustoft').then(function (repos){
   repos.forEach(repo => console.log(repo.name));
 });
+
+
+
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+ReactDom.render(
+<p>Hello, world!</p>,
+  document.getElementById('example')
+);
