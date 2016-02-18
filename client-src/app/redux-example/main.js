88182762ca336bde04dff3e89ddf258c28978c05
Alf Bjørn <abhustoft@gmail.com>
2016-02-18 14:28:33 +0000

    Really added the redux example

---
+++ client-src/app/redux-example/main.js
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './containers/App'
+import reducer from './redux/reducers'
+
+// Code from http://rackt.org/redux/docs/basics/ExampleTodoList.html
+
+let store = createStore(reducer);
+
+//store.subscribe(() => {
+//  console.log(store.getState());
+//});
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
