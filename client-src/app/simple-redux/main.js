811c4cc128b1d91dfae316e2f2b548718b916be5
Alf Bjørn <abhustoft@gmail.com>
2016-02-20 15:52:46 +0100

    More earrange simple redux files

--- client-src/app/simple-redux/main.js
+++ client-src/app/simple-redux/main.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import store from '../store'
-import Todos from './Todos'
+import store from './store'
+import Todos from './components/Todos'
 
 let reactElement = document.getElementById('simple-redux')
 render(
