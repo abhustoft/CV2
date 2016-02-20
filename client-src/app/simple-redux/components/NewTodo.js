0900e3649de39aa3620e76772acf6ff49449c863
Alf Bjørn <abhustoft@gmail.com>
2016-02-20 16:16:50 +0100

    Use class for style change - inline style as example

--- client-src/app/simple-redux/components/NewTodo.js
+++ client-src/app/simple-redux/components/NewTodo.js
@@ -4,12 +4,11 @@ import { connect } from 'react-redux'
 var NewTodo = React.createClass({
   render: function() {
     const {onChange, onClick, editMe} = this.props;
-    const divStyle = {
-      color: editMe.edit ? 'blue' : 'grey'
-    };
+    const colorClass = editMe.edit ? 'blue' : 'grey';
+    const divStyle = {color: colorClass};
     return (
       <div onClick={onClick}>
-        <h3 style={divStyle}>New</h3>
+        <h3 style={divStyle} className={colorClass}>New</h3>
         <input type="text" onKeyUp={onChange}/>
       </div>
     )
