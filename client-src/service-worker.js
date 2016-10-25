f937761d048c53acaa34f5ccb1499e0712bdee84
Alf Bjørn <abhustoft@gmail.com>
2016-10-25 22:30:33 +0200

    Tweak caching

--- client-src/service-worker.js
+++ client-src/service-worker.js
@@ -4,20 +4,19 @@
 var config = {
   version: 'achilles',
   staticCacheItems: [
-    '/app/images/consultant.jpg',
     '/app/css/screen.css',
     '/dist/main.js',
     '/site.js',
     '/'
   ],
-  cachePathPattern: /^\/(?:(20[0-9]{2}|about|blog|css|images|js)\/(.+)?)?$/,
+  cachePathPattern: /^\/(?:(20[0-9]{2}|dist|app|api)\/(.+)?)?$/,
   offlineImage: '<svg role="img" aria-labelledby="offline-title"'
     + ' viewBox="0 0 400 300" xmlns="http://www.w3.org/2000/svg">'
     + '<title id="offline-title">Offline</title>'
     + '<g fill="none" fill-rule="evenodd"><path fill="#D8D8D8" d="M0 0h400v300H0z"/>'
     + '<text fill="#9B9B9B" font-family="Times New Roman,Times,serif" font-size="72" font-weight="bold">'
     + '<tspan x="93" y="172">offline</tspan></text></g></svg>',
-  offlinePage: '/app/offline/offline.html'
+  offlinePage: '/offline/'
 };
 
 function addToCache (cacheKey, request, response) {
@@ -77,6 +76,12 @@ self.addEventListener('fetch', event => {
       isGETRequest      : request.method === 'GET',
       isFromMyOrigin    : url.origin === self.location.origin
     };
+
+    console.log('Check if url: ', url);
+    console.log('matches: ',opts.cachePathPattern);
+    console.log('If true, cache: ', criteria.matchesPathPattern);
+
+
     var failingCriteria    = Object.keys(criteria)
       .filter(criteriaKey => !criteria[criteriaKey]);
     return !failingCriteria.length;
