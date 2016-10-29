f434e5db5d2266b141d3504ae2db7d19db276998
Alf Bjørn <abhustoft@gmail.com>
2016-10-29 18:49:39 +0200

    Remove jspm from gulpfile

--- client-src/gulpfile.js
+++ client-src/gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require('gulp'),
   concat = require('gulp-concat'),
   sourcemaps = require('gulp-sourcemaps'),
   rename = require("gulp-rename"),
-  jspm = require('gulp-jspm'),
   argv = require('yargs').argv,
   uglify = require('gulp-uglify'),
   watch = require('gulp-watch'),
@@ -32,27 +31,6 @@ gulp.task('styl', function () {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(destPath))
     .pipe(gulp.dest(distPath));
-});
-
-gulp.task('bundle', function(){
-
-  if (argv.dist) {
-    return gulp.src('app/main.js')
-      .pipe(jspm({selfExecutingBundle: true}))
-      .pipe(rename("app.min.js"))
-      .pipe(uglify())
-      .pipe(gulp.dest('dist/'));
-  } else {
-    return;
-  }
-});
-
-gulp.task('buildcss', function(){
-  return gulp.src('app/main.js')
-    .pipe(jspm({selfExecutingBundle: true}))
-    .pipe(rename("app.min.js"))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/'));
 });
 
 gulp.task('watch', function() {
